e232c8a4688b35df21c9d89f452383cdae576fd2
bt <btfranzen@gmail.com>
2019-02-10 08:40:26 -0700

    harmlessRansomNote (hash table) practice

--- algorithmPrep.js
+++ algorithmPrep.js
@@ -16,12 +16,48 @@ fizzBuzz(100)
 fizzBuzz(45)
 
 /* -=-=-=-=-=-=-=-=-=-=- */
-// harmless ransom note. 
+// harmless ransom note. practice with hash tables. create a harmless ransom note from the words contained in the magazineText string.
 
+function harmlessRansomNote(noteText, magazineText) {
+  // set all words to lowercase in the note and magazine strings
+  noteText = noteText.toLowerCase()
+  magazineText = magazineText.toLowerCase()
+  // remove any punctuation from both the note and the magazine
+  let regEx =/[!@#$%^&*()-=+:;"'<>,.?/]/g
+  magazineText = magazineText.replace(regEx, '')
+  noteText = noteText.replace(regEx,'')
 
+  // turn the strings into arrays to allow for iteration
+  let noteArr = noteText.split(' ')
+  let magArr = magazineText.split(' ')
+  let magObj = {}
+
+  // create the magazineObject with keys as each word and the values as the count of each word in the magazine
+  magArr.forEach(word => {
+    if(!magObj[word]) magObj[word] = 0
+    magObj[word]++
+  })
+
+  // this will be the boolean returned to tell us whether or not the note can be created from the given magazine.
+  let noteIsPossible = true
+
+  // iterate over each word in the note text and see if enough of those words exist in the magazineObject. each time a word exists in the magazineObject, decrement that word's value. if the word's value is less than 0, then the note is not possible and the noteIsPossible variable is set to false. if the word doesn't exist in the magazineObject, then the noteIsPossible variable is also set to false. if each word in the note exists in the necessary quantities in the magazineObject, then noteIsPossible variable stays true. at the end, return the noteIsPossible variable.
+  noteArr.forEach(word => {
+    if(magObj[word]) {
+      magObj[word]--
+      if(magObj[word] < 0) noteIsPossible = false
+    } else noteIsPossible = false
+  })
+
+  return noteIsPossible
+}
 
 /* -=-=-=- tests -=-=-=- */
+harmlessRansomNote(`this is a secret note for you from a secret admirer`,`Puerto Rico is a place of great wonder and excitement! It has many secret waterfall locations that I'm an admirer of. You must hike quite a distance to fin the secret places as they are far from populated areas; but it is worth the effort. A tip I have for you is to go early in the mornning when it is not so hot out. Also, a note that you must wear hiking boots. This is one of the best places I have ever visited!!!`) // true
 
+harmlessRansomNote('I hate YOU!!!', `Let me tell you?, i hate It whEn I can't find my paNtS!!!`) // true
+
+harmlessRansomNote('I hate YOU, you jerk!!!', `Let me tell you?, i hate It whEn I can't find my paNtS!!!`) // false
 
 
 
