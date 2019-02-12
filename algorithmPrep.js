3d7800f19896b1d3642c5fb50a9ac39a713e2e3a
bt <btfranzen@gmail.com>
2019-02-11 21:48:10 -0700

    2/11 algorithm work

--- algorithmPrep.js
+++ algorithmPrep.js
@@ -1,4 +1,5 @@
 /* -=-=-=-=-=-  -=-=-=-=-=- */
+// repl.it --> https://repl.it/@bfranzen19/fizzBuzz
 // fizz buzz. multiples of 3, print fizz. multiples of 5, print buzz. multiples of both 3 and 5, print fizzBuzz
 
 function fizzBuzz(num) {
@@ -16,6 +17,7 @@ fizzBuzz(100)
 fizzBuzz(45)
 
 /* -=-=-=-=-=-=-=-=-=-=- */
+// repl.it --> https://repl.it/@bfranzen19/harmlessRansomNote
 // harmless ransom note. practice with hash tables. create a harmless ransom note from the words contained in the magazineText string.
 
 function harmlessRansomNote(noteText, magazineText) {
@@ -55,9 +57,63 @@ function harmlessRansomNote(noteText, magazineText) {
 /* -=-=-=- tests -=-=-=- */
 harmlessRansomNote(`this is a secret note for you from a secret admirer`,`Puerto Rico is a place of great wonder and excitement! It has many secret waterfall locations that I'm an admirer of. You must hike quite a distance to fin the secret places as they are far from populated areas; but it is worth the effort. A tip I have for you is to go early in the mornning when it is not so hot out. Also, a note that you must wear hiking boots. This is one of the best places I have ever visited!!!`) // true
 
-harmlessRansomNote('I hate YOU!!!', `Let me tell you?, i hate It whEn I can't find my paNtS!!!`) // true
+harmlessRansomNote('I hate cats!!!', `Let me tell you?, i hate It whEn I can't find my CatS!!!`) // true
 
-harmlessRansomNote('I hate YOU, you jerk!!!', `Let me tell you?, i hate It whEn I can't find my paNtS!!!`) // false
+harmlessRansomNote('I hate cats, you jerk!!!', `Let me tell you?, i hate It whEn I can't find my paNtS!!!`) // false
+
+/* -=-=-=-=-=-=-=-=-=-=- */
+// repl.it --> https://repl.it/@bfranzen19/isPalindrome
+// is it a palindrome? compare a string and return true if the string is the same backwards as forwards, and false if it is not the same backwards and forwards.
+
+// first solution:
+function isPalindrome(string) {
+  let regex = /[a-z]/gi
+  let inputStr = string.toLowerCase().match(regex)
+  let str = inputStr.join('')
+  let rev = inputStr.reverse().join('')
+
+  if(str === rev)return true
+  else return false
+}
+
+/* -=-=-=- tests -=-=-=- */
+isPalindrome('rAcEcAr') // true, testing toLowerCase
+isPalindrome(`Madam, I'm Adam`) // true, testing special chars and uppercase
+isPalindrome('frAnzeN') // false, testing toLowerCase & false
+isPalindrome('R@c3C2r') // true, testing special chars in regex
+
+/* -=-=-=-=-=-=-=-=-=-=- */
+// is palindrome solution 2 - not using regex.
+function isPalindrom(string) {
+  string = string.toLowerCase()
+  let charactersArr = string.split('')
+  let validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('')
+
+  let lettersArr = []
+  charactersArr.forEach(char => {
+    if(validCharacters.indexOf(char) > -1) lettersArr.push(char)
+  })
+
+  if(lettersArr.join('') === lettersArr.reverse().join('')) return true
+  else return false
+}
+
+/* -=-=-=- tests -=-=-=- */
+isPalindrome('rAcEcAr') // true, testing toLowerCase
+isPalindrome(`Madam, I'm Adam`) // true, testing special chars and uppercase
+isPalindrome('frAnzeN') // false, testing toLowerCase & false
+isPalindrome('R@c3C2r') // true, testing special chars
+
+/* -=-=-=-=-=-=-=-=-=-=- */
+//  repl.it --> https://repl.it/@bfranzen19/caesarCipher
+// caesar cipher: passing in 2 parameters string and a number, shift every letter in our given string by the given number. example: zoo keeper, 2 --> bqq mggrgt. z loops around to the beginning of the alphabet to become b, o shifts 2 letters to become q, and so on.
+
+
+
+/* -=-=-=- tests -=-=-=- */
+
+
+
 
 
 
@@ -80,6 +136,7 @@ harmlessRansomNote('I hate YOU, you jerk!!!', `Let me tell you?, i hate It whEn 
 
 /* -=-=-=-=-=- template -=-=-=-=-=- */
 /* -=-=-=-=-=-=-=-=-=-=- */
+// repl.it -->
 //
 
 
